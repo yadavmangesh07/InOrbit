aaa52edd159321299eefdd65d0c90f27f40d1a74
Sachin-Rawat07 <sachinrawatsr07@gmail.com>
2024-10-05 22:36:18 +0530

    sachin made changes

--- Backend/demo.js
+++ Backend/demo.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
 res.send('Jai Mata Di');
 }
 );
+
+app.get('/sachin', (req,res)=>{
+    res.send('hello i am sachin')
+})
 var server = app.listen(8081, function () {
 
 console.log("App is running ")});
