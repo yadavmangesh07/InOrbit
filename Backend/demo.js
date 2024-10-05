25b3cb93aea7258b47bbcabc9bff53179d4eba53
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-10-05 22:42:52 +0530

    Add '/mangesh' endpoint to demo.js

--- Backend/demo.js
+++ Backend/demo.js
@@ -9,6 +9,9 @@ res.send('Jai Mata Di');
 app.get('/sachin', (req,res)=>{
     res.send('hello i am sachin')
 })
+app.get('/mangesh', (req,res)=>{
+    res.send('hello i am Mangesh')
+})
 var server = app.listen(8081, function () {
 
 console.log("App is running ")});
