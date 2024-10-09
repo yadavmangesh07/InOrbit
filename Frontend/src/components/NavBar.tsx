8fd9ffe0a294eb095c167d89ce1a820522b6a230
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-10-09 21:25:05 +0530

    Refactor App component and NavBar, and add ClerkForm component for user sign-in/sign-up & user profile

--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -1,10 +1,18 @@
 import { SignedOut, SignInButton, SignedIn, UserButton } from '@clerk/clerk-react'
 import { ModeToggle } from './ui/mode-toggle'
+import { useTheme } from './ui/Theme-provider';
 
 const NavBar = () => {
+  const { theme } = useTheme();
   return (
-    <div className='ml-[5rem] mr-[5rem] mb-[5rem] flex justify-between '>
-        <div className='flex w-[50%] '>
+    <div
+    className={`ml-[5rem] mr-[5rem] mb-[5rem] flex justify-between p-[.5rem] border-[2px] rounded-sm backdrop-blur ${
+      theme === 'dark'
+        ? 'bg-[rgba(31,31,35,0.1)] text-white border-[rgba(255,255,255,0.5)]'  // 10% opaque dark background and 50% transparent white border
+        : 'bg-[rgba(255,255,255,0.1)] text-black border-[rgba(0,0,0,0.5)]' // 10% opaque light background and 50% transparent black border
+    } bg-opacity-1`}
+>
+        <div className='flex w-[50%] items-center '>
          
          <h1>LOGO</h1>
         </div>
